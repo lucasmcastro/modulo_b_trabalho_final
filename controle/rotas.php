737fed6e63db63043d5ba8e2e677e978eed66362
Lucas Castro <lucasmcastro@gmail.com>
2012-10-17 17:32:54 -0300

    Corrigindo rota padrão quebrada

--- controle/rotas.php
+++ controle/rotas.php
@@ -8,6 +8,10 @@
     $root = $_SERVER['DOCUMENT_ROOT'];
     $templates_dirname = "modulo_b_trabalho_final/templates";
     $templates_path = $root."/".$templates_dirname;
+    if ($resource == '') {
+        $resource = 'alunos';
+        $action = 'index';
+    }
     $final_path = $templates_path."/".$resource."/".$action.".php";
 
     include($final_path);
